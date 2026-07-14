2d85e369febbc2d6dd8a424f667c3625dbf3b404
Prakashgupt <72209238+Prakashgupt@users.noreply.github.com>
2026-07-14 14:31:33 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+     String encodingstring = "-4%";
+    public String encode(List<String> strs) {
+      if(strs.size()==0) return "";
+      String encode= "";
+      for(String s : strs)
+      {  if(s.length()==0)
+        {
+          encode += "empt";
+        }
+         encode += s+encodingstring;
+      }
+      System.out.println(encode);
+      return encode;
+    }
+
+    public List<String> decode(String str) {
+      if(str.length()==0) return new ArrayList<>();
+      String [] decode =str.split(encodingstring);
+      List<String> list = new ArrayList<>();
+      for(String id : decode)
+      {
+         if(id.equals("empt")){list.add("");}
+         else list.add(id);
+      }
+      return list;
+    }
+}
